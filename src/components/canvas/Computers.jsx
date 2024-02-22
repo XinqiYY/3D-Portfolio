b19ff0e50aa6bfb7281fefad76357eb72bcb237f
CometBeautySu <susu.sutianyao@gmail.com>
2024-02-21 17:15:11 -0800

    Updated BallCanvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -56,7 +56,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }} // way to look the 3D model
       gl={{ preserveDrawingBuffer: true }}
