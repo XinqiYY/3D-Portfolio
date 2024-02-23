d5e0ff7c81f50d52289ba70e6bbc246a253dc2a6
CometBeautySu <susu.sutianyao@gmail.com>
2024-02-22 16:42:43 -0800

    Update email sending func

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from "../utils/motion";
 
+// template_snguf1e
+// service_a9joydg
+// EVto5k5rGf2YyPw20
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,41 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value }) 
+  }
 
-  const handleSumbit = (e) => {}
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_a9joydg', 
+      'template_snguf1e',
+      {
+        from_name: form.name,
+        to_name: 'Xinqi',
+        from_email: form.email,
+        to_email: 'xinqiy717595@gmail.com',
+        message: form.message,
+      },
+      'EVto5k5rGf2YyPw20'
+    ).then(() => {
+      setLoading(false);
+      alert("Thank you. I will get back to you as soon as possible.");
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      }, (error) => {
+        setLoading(false);
+        console.log(error);
+        alert('Something went wrong.')
+      })
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
